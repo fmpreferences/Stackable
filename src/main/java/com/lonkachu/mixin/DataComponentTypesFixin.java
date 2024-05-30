99b4fb2973ab83b42cf054105547b37992c93aaf
Colin12345678910 <84333388+Colin12345678910@users.noreply.github.com>
2024-05-30 19:53:41 -0400

    Update to Minecraft 1.21 pre1!
    - Some minor work was needed to bring this mod forward a version due to a crashing issue.

--- src/main/java/com/lonkachu/mixin/DataComponentTypesFixin.java
+++ src/main/java/com/lonkachu/mixin/DataComponentTypesFixin.java
@@ -2,23 +2,9 @@ package com.lonkachu.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.lonkachu.StackableMod;
-import net.minecraft.component.ComponentMap;
-import net.minecraft.component.DataComponentType;
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.AttributeModifiersComponent;
-import net.minecraft.component.type.ItemEnchantmentsComponent;
-import net.minecraft.component.type.LoreComponent;
-import net.minecraft.util.Rarity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.points.BeforeConstant;
-
-import java.lang.annotation.Target;
-
-import static net.minecraft.component.DataComponentTypes.*;
 
 
 @Mixin(DataComponentTypes.class)
